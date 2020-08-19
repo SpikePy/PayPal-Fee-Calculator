20e7229cbff778fb7ad172a2707d5cc382fcd26c
René Hoffmann <hoffelmann@gmail.com>
2020-08-19 15:25:32 +0200

    enable rounding 2 digits

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ fee_fix = 0.35
 fee_var = 0.0249
 
 function js_send() {
-  input_fee.value     = parseFloat(input_send.value) * fee_var + fee_fix
-  input_receive.value = parseFloat(input_send.value) - parseFloat(input_fee.value)
+  input_fee.value     = Math.round((parseFloat(input_send.value) * fee_var + fee_fix) * 100)/100
+  input_receive.value = Math.round((parseFloat(input_send.value) - parseFloat(input_fee.value)) *100)/100
 }
 
 function js_receive() {
-    input_fee.value  = parseFloat(input_receive.value) * fee_var + fee_fix
-    input_send.value = parseFloat(input_receive.value) + parseFloat(input_fee.value)
+    input_fee.value  = Math.round((parseFloat(input_receive.value) * fee_var + fee_fix) * 100)/100
+    input_send.value = Math.round((parseFloat(input_receive.value) + parseFloat(input_fee.value)) * 100)/100
 }
